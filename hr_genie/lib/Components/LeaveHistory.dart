ef192bb7e1499e206dd2a7b4a3775837dee3797d
anasrul03 <anasrul03@gmail.com>
2023-06-22 10:15:23 +0800

    done for the listview builder and model for Leave

--- hr_genie/lib/Components/LeaveHistory.dart
+++ hr_genie/lib/Components/LeaveHistory.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hr_genie/model/LeaveModel.dart';
+import 'package:intl/intl.dart';
 
 class LeaveHistory extends StatefulWidget {
   const LeaveHistory({super.key});
@@ -17,18 +18,44 @@ class _LeaveHistoryState extends State<LeaveHistory> {
         leaveId: "AL003",
         reason: "Going on Vacation",
         applicationStatus: "Pending",
-        attachment: '',
-        employeeId: '',
-        approvedRejectedBy: ''),
+        attachment: 'Hf830nen',
+        employeeId: 'IT302',
+        approvedRejectedBy: 'HRS230'),
+    Leave(
+        startDate: DateTime.parse('2022-07-20 20:18:04Z'),
+        duration: 2.5,
+        leaveTypeId: "AL",
+        leaveId: "AL003",
+        reason: "Going on Vacation",
+        applicationStatus: "Pending",
+        attachment: 'Hf830nen',
+        employeeId: 'IT302',
+        approvedRejectedBy: 'HRS230'),
+    Leave(
+        startDate: DateTime.parse('1969-07-20 20:18:04Z'),
+        duration: 2.5,
+        leaveTypeId: "AL",
+        leaveId: "AL003",
+        reason: "Going on Vacation",
+        applicationStatus: "Pending",
+        attachment: 'Hf830nen',
+        employeeId: 'IT302',
+        approvedRejectedBy: 'HRS230'),
   ];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: leave.length,
         itemBuilder: (context, index) {
+          String dateStart =
+              DateFormat('yyyy-MM-dd HH:mm:ss').format(leave[index].startDate);
+
           return ListTile(
             title: const Text("Annual Leave"),
-            subtitle: Text(leave[index].reason.toString()),
+            subtitle: Text(leave[index].reason),
+            isThreeLine: true,
+            trailing: Text(dateStart),
+            // trailing: Text(leave[index].startDate),
           );
         });
   }
